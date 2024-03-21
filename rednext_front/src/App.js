60ed8eccc2d069ef9e19e4c04214024bbb7c1790
Your Name <you@example.com>
2024-03-22 00:38:33 +0100

    icons

--- rednext_front/src/App.js
+++ rednext_front/src/App.js
@@ -8,7 +8,30 @@ import Topbar from './components/Topbar';
 
 function App() {
     var [points, setPoint]= useState([])
-    points = [{x:51.505, y:0}, {x:51.505, y:0.01}, {x:51.503, y:0.01}]
+    var points = [
+        { x: -22.9068, y: -43.1729 }, // Rio de Janeiro
+        { x: -15.7942, y: -47.8825 }, // Brasília
+        { x: -3.7184, y: -38.5434 },  // Fortaleza
+        { x: -23.5505, y: -46.6333 }, // São Paulo
+        { x: -5.0737, y: -42.8030 },  // Teresina
+        { x: -16.6869, y: -49.2648 }, // Goiânia
+        { x: -25.4284, y: -49.2733 }, // Curitiba
+        { x: -8.0476, y: -34.8770 },  // Recife
+        { x: -30.0346, y: -51.2177 }, // Porto Alegre
+        { x: -12.9714, y: -38.5014 },  // Salvador
+        {x:51.505, y:0}, {x:51.505, y:0.01}, {x:51.503, y:0.01},
+        { x: -7.2307, y: -35.8816 },  // João Pessoa
+        { x: -22.9028, y: -43.2075 }, // Niterói
+        { x: -8.0474, y: -34.8770 },  // Olinda
+        { x: -21.1783, y: -47.8096 }, // Ribeirão Preto
+        { x: -22.9068, y: -43.1729 }, // Rio de Janeiro (Duplicate for variety)
+        { x: -12.9644, y: -38.4959 }, // Lauro de Freitas
+        { x: -27.0982, y: -52.6160 }, // Chapecó
+        { x: -25.4296, y: -49.2713 }, // Curitiba (Duplicate for variety)
+        { x: -22.9035, y: -43.2096 }, // São Gonçalo
+        { x: -8.0638, y: -34.8805 }   // Jaboatão dos Guararapes
+];
+
     const legend_elements = ["a", "b", "c"]
     return (
         <div className="main">

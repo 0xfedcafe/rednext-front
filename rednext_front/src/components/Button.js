50f34755e0ad11f26b54ecae38dad9cdf9362679
SysAtlas <robert@barinov.eu>
2024-03-22 05:17:09 +0100

    Make the popup better!

---
+++ rednext_front/src/components/Button.js
@@ -0,0 +1,7 @@
+const Button = () => {
+    return (
+        <button className="button" />
+    );
+}
+
+export default Button
--- rednext_front/src/components/PopupWindow.js
+++ rednext_front/src/components/PopupWindow.js
@@ -2,48 +2,57 @@ import React, { useState } from 'react';
 import Fab from '@mui/material/Fab';
 import Dialog from '@mui/material/Dialog';
 import { styled } from '@mui/material/styles';
+import Legend from './Legend';
+import { fireIcon, damagedAreaIcon, windIcon, highRiskZoneIcon } from './IconImports';
+import "../styles/Legend.css"
 
 const StyledFab = styled(Fab)(({ theme }) => ({
-  position: 'fixed',
-  bottom: theme.spacing(2),
-  left: theme.spacing(2),
-  zIndex: theme.zIndex.speedDial,
-  width: theme.spacing(5), // Adjust the width to make the button smaller
-  height: theme.spacing(5), // Adjust the height to make the button smaller
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    left: theme.spacing(2),
+    zIndex: theme.zIndex.speedDial,
+    width: theme.spacing(5), // Adjust the width to make the button smaller
+    height: theme.spacing(5), // Adjust the height to make the button smaller
 }));
 
 const PopupContent = styled('div')(({ theme }) => ({
-  width: '60vw', // Set the width to 60% of the viewport width
-  height: '60vh', // Set the height to 60% of the viewport height
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
+    width: '30vw', // Set the width to 60% of the viewport width
+    height: '30vh', // Set the height to 60% of the viewport height
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
 }));
 
 function PopupWindow() {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
+    const legendItems = [
+	{ icon: fireIcon, description: 'Active fire' },
+	{ icon: damagedAreaIcon, description: 'Damaged area' },
+	{ icon: windIcon, description: 'Wind' },
+	{ icon: highRiskZoneIcon, description: 'Zone with high risk of fire' }
+    ];
 
-  const handleOpen = () => {
-    setOpen(true);
-  };
+    const handleOpen = () => {
+	setOpen(true);
+    };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+    const handleClose = () => {
+	setOpen(false);
+    };
 
-  return (
-    <div>
-      <StyledFab color="primary" aria-label="open-popup" onClick={handleOpen}>
-        ⓘ 
-      </StyledFab>
-      <Dialog open={open} onClose={handleClose}>
-        <PopupContent>
-          {/* Content of your popup window */}
-          Your Popup Content Goes Here
-        </PopupContent>
-      </Dialog>
-    </div>
-  );
+    return (
+	<div>
+	<StyledFab color="primary" aria-label="open-popup" onClick={handleOpen}>
+	ⓘ 
+	</StyledFab>
+	<Dialog open={open} onClose={handleClose} >
+	<PopupContent>
+	<h1>Legend:</h1>
+	<Legend items={legendItems} />
+	</PopupContent>
+	</Dialog>
+	</div>
+    );
 }
 
 export default PopupWindow;
